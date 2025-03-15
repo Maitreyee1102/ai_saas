113bc47300a0584c9c0000d0a8b525eb5a65816a
Maitreyee1102 <145358909+Maitreyee1102@users.noreply.github.com>
2025-03-16 01:14:56 +0530

    pricing component ready

--- sommaire/app/page.tsx
+++ sommaire/app/page.tsx
@@ -1,6 +1,8 @@
 import HeroSection from "@/components/home/herosection";
 import BgGradient from "@/components/common/bg-gradient";
-import DemoSection from "@/components/common/demosection";
+import DemoSection from "@/components/home/demosection";
+import HowItWorks from "@/components/home/how-it-works";
+import PricingSection from "@/components/home/pricing-section";
 
 export default function Home() {
   return (
@@ -9,6 +11,8 @@ export default function Home() {
       <div className="flex flex-col">
         <HeroSection/>
         <DemoSection />
+        <HowItWorks/>
+        <PricingSection/>
       </div>
 
     </div>
--- sommaire/components/home/demosection.tsx
+++ sommaire/components/home/demosection.tsx
@@ -4,7 +4,7 @@ export default function DemoSection() {
     return (
         <section className="relative">
             <div className="py-12 lg:py-24 max-w-5xl mx-auto px-4 sm:px-6
-            lg:px-8">
+            lg:px-8 lg:pt-12">
             <div
                 aria-hidden="true"
                 className="pointer-events-none absolute inset-0 -z-10 transform-gpu overflow-hidden blur-3xl"
@@ -18,7 +18,11 @@ export default function DemoSection() {
                 />
             </div>
             <div className='flex-flex-col items-center text-center space-y-4'>
-                <Pizza className="w-6 h-6 text-rose-500" />
+                <div className='inline-flex items-center justify-center p-2 rounded-2xl bg-gray-100/80
+                backdrop-blur-xs border border-gray-500/20 mb-4'>
+                    <Pizza className="w-6 h-6 text-rose-500" />
+                </div>
+                <div className='text-center mb-16'>
                 <h3 className='font-bold text-3xl max-w-2xl mx-auto px-4
                 sm:px-6'>
                     Watch how Sommaire transforms{' '}
@@ -28,6 +32,11 @@ export default function DemoSection() {
                     into an 
                     easy-to-read summary!
                 </h3>
+                </div>
+                <div className='flex justify-center items-center px-2
+                sm:px-4 lg:px-6'>
+                    {/*summary viewer*/}
+                </div>
             </div>
             </div>
         </section>
